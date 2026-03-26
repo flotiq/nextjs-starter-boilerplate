7e3c5208ff32446465b82bedf63a30a033f88831
Dalia <dalia.bachmatiuk@codewave.pl>
2026-03-26 17:03:21 +0100

    UI redesign

--- pages/404.js
+++ pages/404.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import { Header } from 'flotiq-components-react';
+import Head from 'next/head'
+import Image from 'next/image'
 
 const NotFoundPage = () => (
-    <main className="flex flex-col h-screen justify-center items-center">
+    <main className="plant-bg flex flex-col h-screen justify-center items-center">
         <Head>
             <title>Page not found</title>
         </Head>
-        <div className="text-center mt-5 mb-5">
-            <Image src="/Logo.svg" alt="Flotiq" width={300} height={85} />
+
+        <div className="bg-dark-blue/80 h-full w-full flex flex-col justify-center items-center">
+            <div className="relative">
+                <Image
+                    src="/logo-white.svg"
+                    alt="Flotiq"
+                    width={300}
+                    height={85}
+                    className="logo w-60 2xl:w-72 mx-auto"
+                />
+                <h1 className="text-3xl 2xl:text-5xl text-light-blue font-bold text-center mb-4">
+                    Page not found, sorry
+                </h1>
+            </div>
         </div>
-        <Header
-            level={1}
-            className="text-center m-24 text-5xl text-light-blue font-bold"
-        >
-            Page not found, sorry
-        </Header>
     </main>
-);
+)
 
 export default NotFoundPage;
